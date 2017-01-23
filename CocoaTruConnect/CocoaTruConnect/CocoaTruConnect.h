3d3733722c2fe660c0581c70faa79f08a34c5e3e
Hal Mueller <hal@mobilegeographics.com>
2017-01-22 19:34:06 -0800

    Basic structure setup.

---
+++ CocoaTruConnect/CocoaTruConnect/CocoaTruConnect.h
@@ -0,0 +1,19 @@
+//
+//  CocoaTruConnect.h
+//  CocoaTruConnect
+//
+//  Created by Hal Mueller on 1/22/17.
+//  Copyright © 2017 Hal Mueller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaTruConnect.
+FOUNDATION_EXPORT double CocoaTruConnectVersionNumber;
+
+//! Project version string for CocoaTruConnect.
+FOUNDATION_EXPORT const unsigned char CocoaTruConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTruConnect/PublicHeader.h>
+
+

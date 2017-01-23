343f01d1bfcae46ec8d44a1559af27785b7976d9
Hal Mueller <hal@mobilegeographics.com>
2017-01-22 22:02:44 -0800

    Add BLE service/characteristic UUIDs.

--- CocoaTruConnect/Source/CTCManager.swift
+++ CocoaTruConnect/Source/CTCManager.swift
@@ -9,22 +9,32 @@
 import CoreFoundation
 import CoreBluetooth
 
-let FUEL_SENSOR_SERVICE_UUID_STRING =        "be14e552-dd6d-a586-ce42-c1d900006eca"
-let FUEL_SENSOR_PULSE_COUNT_CHARACTERISTIC_UUID_STRING = "be14e553-dd6d-a586-ce42-c1d900006eca"
-let fuelSensorServiceUUID = CBUUID(string: FUEL_SENSOR_SERVICE_UUID_STRING)
-let fuelSensorPulseCountCharacteristicUUID = CBUUID(string: FUEL_SENSOR_PULSE_COUNT_CHARACTERISTIC_UUID_STRING)
+// from http://truconnect.ack.me/2.0/ble_connections
 
-let BATTERY_SERVICE_UUID_STRING = "180F"
-let BATTERY_LEVEL_CHARACTERISTIC_UUID_STRING = "2A19"
-let batteryServiceUUID = CBUUID(string: BATTERY_SERVICE_UUID_STRING)
-let batteryLevelCharacteristicUUID = CBUUID(string: BATTERY_LEVEL_CHARACTERISTIC_UUID_STRING)
+let SERVICE_TRUCONNECT_UUID_STRING = "175f8f23-a570-49bd-9627-815a6a27de2a"
 
+// Strings sent to TruConnect serial interface
+let CHARACTERISTIC_TRUCONNECT_PERIPHERAL_RX_UUID_STRING = "1cce1ea8-bd34-4813-a00a-c76e028fadcb"
+
+// Strings received from TruConnect serial interface
+let CHARACTERISTIC_TRUCONNECT_PERIPHERAL_TX_UUID_STRING = "cacc07ff-ffff-4c48-8fae-a9ef71b75e26"
+
+// 1 (STREAM_MODE), 2 (LOCAL_COMMAND_MODE), 3 (REMOTE_COMMAND_MODE)
+let CHARACTERISTIC_TRUCONNECT_MODE_UUID_STRING =          "20b9794f-da1a-4d14-8014-a0fb9cefb2f7"
+
+let truconnectServiceUUID = CBUUID(string: SERVICE_TRUCONNECT_UUID_STRING)
+let truconnectPeripheralRXCharacteristicUUID =   CBUUID(string: CHARACTERISTIC_TRUCONNECT_PERIPHERAL_RX_UUID_STRING)
+let truconnectPeripheralTXCharacteristicUUID =   CBUUID(string: CHARACTERISTIC_TRUCONNECT_PERIPHERAL_TX_UUID_STRING)
+let truconnectPeripheralModeCharacteristicUUID = CBUUID(string: CHARACTERISTIC_TRUCONNECT_MODE_UUID_STRING)
+
+// standard services
 // source: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.device_information.xml
-
-let deviceInformationServiceUUID = CBUUID(string: "180A")
+let batteryServiceUUID =                 CBUUID(string: "180F")
+let batteryLevelCharacteristicUUID =     CBUUID(string: "2A19")
+let deviceInformationServiceUUID =       CBUUID(string: "180A")
 let manufacturerNameCharacteristicUUID = CBUUID(string: "2A29")
-let modelNumberCharacteristicUUID = CBUUID(string: "2A24")
-let serialNumberCharacteristicUUID = CBUUID(string: "2A25")
+let modelNumberCharacteristicUUID =      CBUUID(string: "2A24")
+let serialNumberCharacteristicUUID =     CBUUID(string: "2A25")
 let hardwareRevisionCharacteristicUUID = CBUUID(string: "2A27")
 let softwareRevisionCharacteristicUUID = CBUUID(string: "2A28")
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CocoaTruConnect
 Cocoa library to talk to the Zentri TruConnect Bluetooth LE-RS232 adapter.
+
+This sensor is sold by SerialIO.
+
+Documentation on the Bluetooth LE services and characteristics is at http://truconnect.ack.me/2.0/ble_connections.
